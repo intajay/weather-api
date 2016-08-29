a4425dff6cb8b296e36878a9dbf06f08d1dedd8d
intajay <kataravat@gmail.com>
2016-08-30 00:54:36 +0530

    Add API Key

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ module.exports = function(location, callback) {
     if (!location.lat || !location.lon) {
         return callback('No location detected!');
     } else {
-        var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + location.lat + '&lon=' + location.lon + '&APPID=YOUR-API-KEY';
+        var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + location.lat + '&lon=' + location.lon + '&APPID=afc42b23b09adb2ce87e9e67113b3d32';
         request({
             url: url,
             json: true
